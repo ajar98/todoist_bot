c8c81837a0cd6fc3a2944bceae8b004dabca2c7b
Ajay Raj <ajay@Ajays-MacBook-Pro.local>
2016-06-16 00:00:28 -0700

    take 2

--- todoist.py
+++ todoist.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 APP_TOKEN = os.environ['FB_ACCESS_TOKEN']
 FB_MESSAGES_ENDPOINT = "https://graph.facebook.com/v2.6/me/messages"
 
+
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
     if request.method == "GET":
@@ -16,20 +17,23 @@ def webhook():
             return 'Wrong validation token'
     else:
         if request.method == "POST":
-            data = request.json()
-            sender_id = data["entry"][0]["messaging"][0]["sender"]["id"]
-			send_back_to_fb = {
-		        "recipient": {
-		            "id": sender_id,
-		        },
-		        "message": "this is a test response message"
-		    }
-            fb_response = requests.post(FB_MESSAGES_ENDPOINT,
-                                params={"access_token": FB_TOKEN},
-                                data=send_back_to_fb)
-            if not fb_response.ok:
-            	print 'jeepers. %s: %s' % (fb_response.status_code, fb_response.text)
+            data = json.loads(request.data)['entry'][0]['messaging']
+            for m in data:
+                resp_id = m['sender']['id']
+                resp_mess = {
+                    'recipient': {
+                        'id': resp_id
+                    },
+                    'message': {
+                        'text': m['message']['text'],
+                    }
+                }
+                fb_response = requests.post(
+                    FB_MESSAGES_ENDPOINT,
+                    params={"access_token": APP_TOKEN},
+                    data=json.dumps(resp_mess),
+                    headers={'content-type': 'application/json'})
         return "OK", 200
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
