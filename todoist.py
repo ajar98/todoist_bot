c4720267ccd93b68e434adfef1d8eb6251fd0fb2
Ajay Raj <ajay@Ajays-MacBook-Pro.local>
2016-06-15 23:27:43 -0700

    FB webhook

--- todoist.py
+++ todoist.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
@@ -6,9 +7,9 @@ app = Flask(__name__)
 def webhook():
     if request.method == "GET":
         if request.args.get('hub.verify_token') == 'todoist_is_my_fave':
-            return Response(request.args.get('hub.challenge'))
+            return request.args.get('hub.challenge')
         else:
-            return Response('Wrong validation token')
+            return 'Wrong validation token'
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=5000)
