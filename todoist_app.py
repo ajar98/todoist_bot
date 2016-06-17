d7cfae8f55b3eff81829840f7d560127a4ae6e40
Ajay Raj <ajay@Ajays-MacBook-Pro.local>
2016-06-16 17:26:32 -0700

    tasks functionality

--- todoist_app.py
+++ todoist_app.py
@@ -10,6 +10,7 @@ APP_TOKEN = "EAAPGnoxDIZC8BAGJC26eKJnuOsHO95ZCqmDvxOY0OoLHUSjecSsZBUObB\
 PYjpLhLzjjv8MdWrSvsYZAAkG6XO3Vx9S54OYHZCFe7nV04pBBhyYK8VPwehszO44W57l6s\
 EbTemYEO4cesUW19ZB2GxAZB1CJuaguPfqlmrtLCzlplgZDZD"
 FB_MESSAGES_ENDPOINT = "https://graph.facebook.com/v2.6/me/messages"
+TOKEN = 'e5d57a2cbb3a10c236c78eba3d578e18f875a4ee'
 
 
 @app.route('/webhook', methods=['GET', 'POST'])
@@ -49,7 +50,7 @@ def webhook():
 
 
 def get_response(message):
-    tc = TodoistClient()
+    tc = TodoistClient(TOKEN)
     if message == 'tasks':
         return '\n'.join(
             ['* {0}'.format(task) for task in tc.get_this_week_tasks()]
