02d8a6aa481dcf4d33c7adbd76ce2a79409be04a
Ajay Raj <ajay@Ajays-MacBook-Pro.local>
2016-06-20 01:01:31 -0700

    access token

--- todoist_app.py
+++ todoist_app.py
@@ -14,7 +14,8 @@ APP_TOKEN = "EAAPGnoxDIZC8BAGJC26eKJnuOsHO95ZCqmDvxOY0OoLHUSjecSsZBUObB\
 PYjpLhLzjjv8MdWrSvsYZAAkG6XO3Vx9S54OYHZCFe7nV04pBBhyYK8VPwehszO44W57l6s\
 EbTemYEO4cesUW19ZB2GxAZB1CJuaguPfqlmrtLCzlplgZDZD"
 FB_MESSAGES_ENDPOINT = "https://graph.facebook.com/v2.6/me/messages"
-OAUTH_ENDPOINT = "https://todoist.com/oauth/authorize?"
+OAUTH_CODE_ENDPOINT = "https://todoist.com/oauth/authorize?"
+OAUTH_ACCESS_TOKEN_ENDPOINT = "https://todoist.com/oauth/access_token"
 TODOIST_ACCESS_TOKEN = None
 REDIRECT_URI = "https://www.pure-hamlet-63323.herokuapp.com/todoist_callback"
 
@@ -34,6 +35,8 @@ def webhook():
                 if ('message' in m) and ('text' in m['message']):
                     sender_id = m['sender']['id']
                     message = m['message']['text']
+                    if not(TODOIST_ACCESS_TOKEN):
+                        get_access_token(sender_id)
                     bot_responses = get_bot_responses(sender_id, message)
                     for bot_response in bot_responses:
                         print bot_response
@@ -42,25 +45,24 @@ def webhook():
 
 
 def get_bot_responses(sender_id, message):
-    # while not(TODOIST_ACCESS_TOKEN):
-    get_access_token(sender_id)
-    tc = TodoistClient(TODOIST_ACCESS_TOKEN)
-    if message == 'tasks':
-        return ['* {0} (Due {1})'.format(
-            task['content'],
-            task['date_string']) for task in tc.get_this_week_tasks()]
-    elif 'write task' in message:
-        task_name = message.split('\"')[1]
-        date_string = message.split('\"')[3]
-        tc.write_task(task_name, 'Inbox', date_string=date_string)
-        return ['Task written.']
-    else:
-        return ['Type \'tasks\' to get your tasks for the next week. \
-        Type \'write task \"<task_name>\" due \"<date_string>\"\'']
+    if TODOIST_ACCESS_TOKEN:
+        tc = TodoistClient(TODOIST_ACCESS_TOKEN)
+        if message == 'tasks':
+            return ['* {0} (Due {1})'.format(
+                task['content'],
+                task['date_string']) for task in tc.get_this_week_tasks()]
+        elif 'write task' in message:
+            task_name = message.split('\"')[1]
+            date_string = message.split('\"')[3]
+            tc.write_task(task_name, 'Inbox', date_string=date_string)
+            return ['Task written.']
+        else:
+            return ['Type \'tasks\' to get your tasks for the next week. \
+            Type \'write task \"<task_name>\" due \"<date_string>\"\'']
 
 
 def get_access_token(sender_id):
-    authorization_url = OAUTH_ENDPOINT + \
+    authorization_url = OAUTH_CODE_ENDPOINT + \
         urllib.urlencode(
             {
                 'client_id': os.environ['TODOIST_CLIENT_ID'],
@@ -74,6 +76,32 @@ def get_access_token(sender_id):
         'Authorize now',
         authorization_url
     )
+
+
+@app.route('/todoist_callback')
+def todoist_callback(methods=['GET', 'POST']):
+    error = request.args.get('error', '')
+    if error:
+        return "Error: " + error
+    state = request.args.get('state', '')
+    if not is_valid_state(state):
+        # Uh-oh, this request wasn't started by us!
+        abort(403)
+    code = request.args.get('code')
+    # We'll change this next line in just a moment
+    TODOIST_ACCESS_TOKEN = get_token(code)
+
+
+def get_token(code):
+    client_auth = requests.auth.HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
+    post_data = {"grant_type": "authorization_code",
+                 "code": code,
+                 "redirect_uri": REDIRECT_URI}
+    response = requests.post(OAUTH_ACCESS_TOKEN_ENDPOINT,
+                             auth=client_auth,
+                             data=post_data)
+    token_json = response.json()
+    return token_json["access_token"]
 
 
 def send_FB_message(sender_id, message):
@@ -127,13 +155,6 @@ def send_FB_button(sender_id, text, button_text, web_url):
             }
         }
     )
-
-
-@app.route('/todoist_callback')
-def todoist_callback(methods=['GET', 'POST']):
-    print request.data
-    if 'access_token' in request.data:
-        TODOIST_ACCESS_TOKEN = request.data['access_token']
 
 
 if __name__ == "__main__":
