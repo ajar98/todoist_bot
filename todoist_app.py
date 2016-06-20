0364d8fcc00da0c3717ce8b32553d8bc32222868
Ajay Raj <ajay@Ajays-MacBook-Pro.local>
2016-06-20 00:51:02 -0700

    stop the madness

--- todoist_app.py
+++ todoist_app.py
@@ -42,8 +42,8 @@ def webhook():
 
 
 def get_bot_responses(sender_id, message):
-    while not(TODOIST_ACCESS_TOKEN):
-        get_access_token(sender_id)
+    # while not(TODOIST_ACCESS_TOKEN):
+    get_access_token(sender_id)
     tc = TodoistClient(TODOIST_ACCESS_TOKEN)
     if message == 'tasks':
         return ['* {0} (Due {1})'.format(
