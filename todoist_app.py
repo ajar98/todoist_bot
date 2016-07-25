e270aef9ddb1d930e9b42a40f723ca7f6c85fa3a
Ajay Raj <ajay@Ajays-MacBook-Pro.local>
2016-07-24 19:25:50 -0700

    tasks in project error

--- todoist_app.py
+++ todoist_app.py
@@ -125,7 +125,11 @@ def webhook():
                                 project_tasks = tc.get_project_tasks(
                                     project_name
                                 )
-                                print project_tasks
+                                send_tasks(
+                                    sender_id,
+                                    project_tasks,
+                                    tc.tz_info['hours']
+                                )
                                 # if type(project_tasks) is list:
                                 #     if len(project_tasks) > 0:
                                 #         send_tasks(
